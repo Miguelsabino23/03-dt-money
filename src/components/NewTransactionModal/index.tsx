f40f15fa649d3003da8e5ffd35e8c86d78920c7f
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-07 18:31:07 -0300

    feat: adicionar componentes CloseButton, TransactionType e TransactionTypeButton ao componente NewTransactionModal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,12 @@
 import * as Dialog from "@radix-ui/react-dialog";
-import { Content, Overlay } from "./style";
+import {
+  CloseButton,
+  Content,
+  Overlay,
+  TransactionType,
+  TransactionTypeButton,
+} from "./style";
+import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
 
 export function NewTransactionModal() {
   return (
@@ -9,15 +16,29 @@ export function NewTransactionModal() {
       <Content>
         <Dialog.Title>Nova Transação</Dialog.Title>
 
+        <CloseButton>
+          <X size={24} />
+        </CloseButton>
+
         <form action=''>
           <input type='text' placeholder='Descrição' required />
           <input type='number' placeholder='Preço' required />
           <input type='text' placeholder='Categoria' required />
 
+          <TransactionType>
+            <TransactionTypeButton variant='income' value='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+
+            <TransactionTypeButton variant='outcome' value='outcome'>
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type='submit'>Cadastrar</button>
         </form>
-
-        <Dialog.Close />
       </Content>
     </Dialog.Portal>
   );
