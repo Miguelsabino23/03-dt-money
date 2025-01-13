6862751067c04cc6b11254bfd730c8c7ebc5b00a
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-13 17:50:23 -0300

    feat: adicionar priceFormatter e hook sumary

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -4,11 +4,11 @@ import {
   CurrencyDollar,
 } from "@phosphor-icons/react";
 import { SummaryCard, SummaryContainer } from "./style";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { useContext } from "react";
+import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -18,7 +18,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -27,7 +27,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant='green'>
@@ -36,7 +36,7 @@ export function Summary() {
           <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   );
