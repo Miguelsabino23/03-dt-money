8f1a6a2622a500d33b0933708226377b1e32ca18
miguelsabino23 <miguel2012palhada@gmail.com>
2025-01-16 20:58:14 -0300

    feat: adicionando novos componentes para responsividade mobile

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -3,12 +3,18 @@ import {
   ArrowCircleUp,
   CurrencyDollar,
 } from "@phosphor-icons/react";
-import { SummaryCard, SummaryContainer } from "./style";
-import { priceFormatter } from "../../utils/formatter";
+import { DateTask, SummaryCard, SummaryContainer } from "./style";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 import { useSummary } from "../../hooks/useSummary";
+import { useContextSelector } from "use-context-selector";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
   const summary = useSummary();
+
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <SummaryContainer>
@@ -19,6 +25,18 @@ export function Summary() {
         </header>
 
         <strong>{priceFormatter.format(summary.income)}</strong>
+        {(() => {
+          const lastIncome = transactions
+            .filter((transaction) => transaction.type === "income")
+            .pop();
+
+          return lastIncome ? (
+            <DateTask>
+              Última entrada em{" "}
+              {dateFormatter.format(new Date(lastIncome.createdAt))}
+            </DateTask>
+          ) : null;
+        })()}
       </SummaryCard>
 
       <SummaryCard>
@@ -28,6 +46,18 @@ export function Summary() {
         </header>
 
         <strong>{priceFormatter.format(summary.outcome)}</strong>
+        {(() => {
+          const lastOutcome = transactions
+            .filter((transaction) => transaction.type === "outcome")
+            .pop();
+
+          return lastOutcome ? (
+            <DateTask>
+              Última entrada em{" "}
+              {dateFormatter.format(new Date(lastOutcome.createdAt))}
+            </DateTask>
+          ) : null;
+        })()}
       </SummaryCard>
 
       <SummaryCard variant='green'>
